84cb4c18bd3d216559d25cd67f430f8d8edd32c2
Gary Yeh <cashbooktw@gmail.com>
2016-04-23 21:29:56 +0800

    add traditional chinese site name

--- server/public/index.js
+++ server/public/index.js
@@ -20,7 +20,22 @@
 // request.send();
 var all_site_status = "{";
 var count = 0;
-
+var site = [101,102,103,104,105,106,107,108,109,110,
+            111,112,113,114,115,116,117,118,119,120,
+            121,122,123,124,
+            201,202,203,204,205,206,207,208,209,210,
+            211,212,213,214,
+            999];//39
+var cht_site = ["小港","高雄國際機場","草衙","前鎮高中","凱旋",
+                "獅甲","三多商圈","中央公園","美麗島","高雄車站",
+                "後驛","凹子底","巨蛋","生態園區","左營",
+                "世運","油廠國小","楠梓加工區","後勁","都會公園",
+                "青埔","橋頭糖廠","橋頭火車站","南崗山",
+                "西子灣","鹽埕埔","市議會","美麗島","信義國小",
+                "文化中心","五塊厝","技擊館","衛武營","鳳山西站",
+                "鳳山","大東","鳳山國中","大寮",
+                "美麗島"
+];
 var sendRequest = function(){
   var request = new XMLHttpRequest();
   request.open('GET', 'data.json', true);
@@ -76,18 +91,12 @@ var newSendRequest = function(theSite){
   };
   request.send();
 };
-var site = [101,102,103,104,105,106,107,108,109,110,
-            111,112,113,114,115,116,117,118,119,120,
-            121,122,123,124,
-            201,202,203,204,205,206,207,208,209,210,
-            211,212,213,214,
-            999];//39
 
 var parseJSON = function (resp){
   resp = JSON.parse(resp);
   for(var i = 0; i < 39; i++){
     console.log(site[i]);
-    document.getElementById(site[i]).innerHTML = site[i];
+    document.getElementById(site[i]).innerHTML = cht_site[i];
     document.getElementById(site[i] + "-0-descr").innerHTML = resp[site[i]][0].descr;
     document.getElementById(site[i] + "-0-arrival").innerHTML = resp[site[i]][0].arrival;
     document.getElementById(site[i] + "-0-next_arrival").innerHTML = resp[site[i]][0].next_arrival;
